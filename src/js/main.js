9c5cd577bcdfd3317d8b4ed8fdb611486160c6d9
Marina Vykhristiuk <marina.bugaeva.v@gmail.com>
2017-04-29 01:02:05 +0300

    переход между страницами

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,56 @@
 //наведение курсора на логотип и последующая смена цвета макета
-$('.logo').mouseenter(function () {
+// $('.logo').mouseenter(function () {
+//
+//   var arrColor = ["#1a48a6", "#a71b3b", "#a6781a", "#1ba768"];
+//   var randColor = Math.floor(Math.random() * arrColor.length);
+//
+//   var color = arrColor[randColor];
+//   console.log(color);
+//
+//   $('.page-footer').css({"background-color": color});
+//
+//   $('.main-nav__item-name').css({"background-color": color});
+//
+//   $('.logo').mouseout(function () {
+//     $('.page-footer').css({"background-color": this.color});
+//     $('.main-nav__item-name').css({"background-color": this.color});
+//   });
+// });
 
-  var arrColor = ["#1a48a6", "#a71b3b", "#a6781a", "#1ba768"];
-  var randColor = Math.floor(Math.random() * arrColor.length);
 
-  var color = arrColor[randColor];
-  console.log(color);
+function clickLink() {
+  var link = document.getElementsByClassName('main-nav__item-menu');
 
-  $('.page-footer').css({"background-color": color});
+  for (var i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', request);
+  }
+}
 
-  $('.main-nav__item-name').css({"background-color": color});
+clickLink();
 
-  $('.logo').mouseout(function () {
-    $('.page-footer').css({"background-color": this.color});
-    $('.main-nav__item-name').css({"background-color": this.color});
+function request() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', './content1.html', true);
+  xhr.addEventListener('load', function () {
+    if (xhr.status != 200) {
+      alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+    } else {
+      var content = xhr.responseText;
+    }
   });
-});
+  xhr.send();
+}
+
+
+function extractContent() {
+
+  var htmlString = content;
+
+  var beginningContent = htmlString.indexOf("//content1Start") + "//content1Start".length;
+  var endContent = htmlString.indexOf("//content1End");
+  var extractedContent = htmlString.substr(beginningContent, endContent - beginningContent).trim();
+
+
+  alert(extractedContent);
+}
+
