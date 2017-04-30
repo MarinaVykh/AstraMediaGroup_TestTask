af9a8735246bc71d4840bf8f229307ded2a5d004
Marina Vykhristiuk <marina.bugaeva.v@gmail.com>
2017-05-01 02:28:08 +0300

    saving playground

--- src/js/main.js
+++ src/js/main.js
@@ -17,41 +17,89 @@
 //   });
 // });
 
+//
+// function clickLink() {
+//   var links = document.getElementsByClassName('main-nav__item-menu');
+//
+//   for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener('click', request);
+//   }
+// }
+//
+// //clickLink();
+//
+// function request() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', './content1.html', true);
+//   xhr.addEventListener('load', function () {
+//     if (xhr.status != 200) {
+//       alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+//     } else {
+//       var content = xhr.responseText;
+//     }
+//
+//     //alert(extractContent(content));
+//
+//
+//   });
+//   xhr.send();
+// }
+//
+//
+// function extractContent(a) {
+//   var htmlString = a;
+//
+//   var beginningContent = htmlString.indexOf("//content1Start") + "//content1Start".length;
+//   var endContent = htmlString.indexOf("//content1End");
+//   var extractedContent = htmlString.substr(beginningContent, endContent - beginningContent).trim();
+//
+//   return extractedContent;
+//
+// }
 
-function clickLink() {
-  var link = document.getElementsByClassName('main-nav__item-menu');
 
-  for (var i = 0; i < link.length; i++) {
-    link[i].addEventListener('click', request);
-  }
-}
+var links = document.getElementsByClassName('main-nav__item-menu');
 
-clickLink();
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function (event) {
 
-function request() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', './content1.html', true);
-  xhr.addEventListener('load', function () {
-    if (xhr.status != 200) {
-      alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-    } else {
-      var content = xhr.responseText;
+    var flippers = document.getElementsByClassName('main-nav__item-wrapper-flipper');
+    for (var i = 0; i < links.length; i++) {
+      var doesContainElement = flippers[i].contains(event.target);
+      if (doesContainElement == false) {
+        flippers[i].style.display = 'none';
+        document.body.style.position = 'static';
+      }
     }
 
-    alert(extractContent(content));
-
   });
-  xhr.send();
 }
 
 
-function extractContent(a) {
-  var htmlString = a;
+// var block = document.getElementById('b1');
+//
+// document.getElementById('b1').onclick = function(event) {
+//   var target = event.target;
+//
+//
+// target.style.display='none';
+//
+// }
 
-  var beginningContent = htmlString.indexOf("//content1Start") + "//content1Start".length;
-  var endContent = htmlString.indexOf("//content1End");
-  var extractedContent = htmlString.substr(beginningContent, endContent - beginningContent).trim();
 
-  return extractedContent;
-}
+// var links = document.getElementsByClassName('main-nav__item-menu');
+//
+// for (var i = 0; i < links.length; i++) {
+//   links[i].addEventListener('click', function () {
+//
+//
+//     var t = document.getElementsByClassName('main-nav__item-wrapper-flipper');
+//     for (var i = 0; i < links.length; i++) {
+//       t[i].style.display = 'none';
+//       document.body.style.position = 'static';
+//     }
+//
+//   });
+// }
+
 
